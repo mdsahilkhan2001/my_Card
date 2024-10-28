57caf3fff1d20f48a0ccb3f135113e34f95af573
MD Sahil Khan <mdsahilkhan2001@gmail.com>
2024-10-28 18:08:00 +0530

    Card

---
+++ my-project/src/App.jsx
@@ -0,0 +1,19 @@
+ 
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+    <h1 className='bg-gray-400 text-black p-4 rounded-xl'  >Tailwind test</h1>
+    <button class="bg-red-300 hover:bg-sky-700 ...">
+  Save changes
+</button>
+ 
+
+      </>
+  )
+}
+
+export default App
